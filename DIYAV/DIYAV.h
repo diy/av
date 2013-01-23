431cde2ae9f730ebe456b42e3309e73897c7c54c
Jonathan Beilin <jbeilin@gmail.com>
2013-01-22 17:05:27 -0800

    Project re-org

---
+++ DIYAV/DIYAV.h
@@ -0,0 +1,13 @@
+//
+//  DIYAV.h
+//  DIYAV
+//
+//  Created by Jonathan Beilin on 1/22/13.
+//  Copyright (c) 2013 DIY. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DIYAV : NSObject
+
+@end
---
+++ DIYAV/DIYAV.m
@@ -0,0 +1,13 @@
+//
+//  DIYAV.m
+//  DIYAV
+//
+//  Created by Jonathan Beilin on 1/22/13.
+//  Copyright (c) 2013 DIY. All rights reserved.
+//
+
+#import "DIYAV.h"
+
+@implementation DIYAV
+
+@end
